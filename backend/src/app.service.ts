017137626669b7aa7afe4a4a9a0ce311abb71148
Iosdan Ferreira <iosdan.ferreira@gmail.com>
2024-03-26 19:06:13 -0300

    Fix the developer container

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World eee!';
+    return 'Hello World eessdsde!d salvo';
   }
 }
